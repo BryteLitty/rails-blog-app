67efc26d107c5eafebcf2bcc0dc4c35fc5349ae8
BryteLitty <littyofficials@gmail.com>
2023-08-16 07:48:56 +0000

    resolve changes

--- db/migrate/20230816065904_add_author_to_posts.rb
+++ db/migrate/20230816065904_add_author_to_posts.rb
@@ -1,6 +1,5 @@
 class AddAuthorToPosts < ActiveRecord::Migration[7.0]
   def change
-    add_reference :posts, :author, foreign_key: { to_table: 'users' } 
+    add_reference :posts, :author, null: false, foreign_key: true
   end
 end
-
