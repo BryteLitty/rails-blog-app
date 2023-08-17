2225eb3a3afd3e91e2b32b35f2c4e81e5b71923b
BryteLitty <littyofficials@gmail.com>
2023-08-17 11:39:04 +0000

    add models

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,8 @@
 class Like < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: 'User'
   belongs_to :post
+
+  def like_counter
+    post.update(likes_counter: post.likes.all.length)
+  end
 end
