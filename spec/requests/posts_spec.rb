aa35494ea8f60afb21670706dacb6bc77d2266fe
Bright Kportiklah <littyofficials@gmail.com>
2023-08-23 22:42:30 +0000

    update changes

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
+  # for the index template
   describe 'GET /users/:id/posts' do
     it 'the response status is correct' do
       get '/users/1/posts'
@@ -10,6 +11,24 @@ RSpec.describe 'Posts', type: :request do
     it 'renders the correct index page' do
       get '/users/1/posts'
       expect(response).to render_template(:index)
+    end
+
+    it 'includes the correct placeholder text' do
+      get '/users/1/posts'
+      expect(response.body).to include('Here is a list of posts for a given user 1')
+    end
+  end
+
+  # for the show template
+  describe 'GET /users/:id/posts/:post_id' do
+    it 'the response status is correct' do
+      get '/users/1/posts/2'
+      expect(response.status).to eq(200)
+    end
+
+    it 'renders the correct show template' do
+      get '/users/1/posts/3'
+      expect(response.body).to render_template(:show)
     end
 
     it 'includes the correct placeholder text' do
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
+  # for the index template
   describe 'GET /users' do
+    it 'response status is correct' do
+      get '/users'
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the correct index template' do
+      get '/users'
+      expect(response).to render_template(:index)
+    end
+
+    it 'includes the correct placeholder text' do
+      get '/users'
+      expect(response.body).to include('List of users')
+    end
+  end
+
+  # for the show template
+  describe 'GET #show' do
     it 'returns http success' do
       get '/users/show'
       expect(response).to have_http_status(:success)
