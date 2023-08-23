968275e628b79b7bb0f83374b60782c8e252eb37
Bright Kportiklah <littyofficials@gmail.com>
2023-08-23 21:32:58 +0000

    added request specs

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'GET /users/:id/posts' do
+    it 'the response status is correct' do
+      get '/users/1/posts'
+      expect(response.status).to eq(200)
+    end
+
+    it 'renders the correct index page' do
+      get '/users/1/posts'
+      expect(response).to render_template(:index)
+    end
+
+    it 'includes the correct placeholder text' do
+      get '/users/1/posts'
+      expect(response.body).to include('Here is a list of posts for a given user 1')
+    end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,10 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /show' do
+  describe 'GET /users' do
     it 'returns http success' do
       get '/users/show'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the correct show page' do
+      get '/users/1'
+      expect(response).to render_template(:show)
+    end
+
+    it 'includes the correct placeholder text' do
+      get '/users/1'
+      expect(response.body).to include('Users ID: 1')
     end
   end
 end
