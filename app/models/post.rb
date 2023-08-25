566bcf900aea0c47c3a9a9ba30758aa3ba185c2c
Bright Kportiklah <littyofficials@gmail.com>
2023-08-25 22:14:26 +0000

    update with requested changes

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
 
   before_create :post_count
+  after_create :update_comments_counter
 
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
