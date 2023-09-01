332be52f5ebc52f54659c735909b348702dc31f4
Bright Kportiklah <littyofficials@gmail.com>
2023-09-01 10:04:24 +0000

    fix linter error

--- spec/features/post_index_spec.rb
+++ spec/features/post_index_spec.rb
@@ -1,12 +1,14 @@
 require 'rails_helper'
+
 RSpec.feature 'Post Index', type: :feature do
-  let(:user) { User.create(name: 'Tom', photo: 'https://www.kasandbox.org/programming-images/avatars/leaf-blue.png', bio: 'He is a good programmar') }
+  let(:user) { User.create(name: 'Tom', photo: 'https://www.kasandbox.org/programming-images/avatars/leaf-blue.png', bio: 'He is a good programmer') }
   let!(:post) { Post.create(author: user, title: "first post's title", text: 'first text') }
-  let!(:comment1) { Comment.create(author: user, post:, text: 'first comment') }
-  let!(:comment2) { Comment.create(author: user, post:, text: 'second comment') }
-  let!(:comment3) { Comment.create(author: user, post:, text: 'third comment') }
-  let!(:like1) { Like.create(author: user, post:) }
-  scenario "see user's profile picture, username, number of posts and interactions" do
+  let!(:comment1) { Comment.create(author: user, post: post, text: 'first comment') }
+  let!(:comment2) { Comment.create(author: user, post: post, text: 'second comment') }
+  let!(:comment3) { Comment.create(author: user, post: post, text: 'third comment') }
+  let!(:like1) { Like.create(author: user, post: post) }
+
+  scenario "see user's profile picture, username, number of posts, and interactions" do
     visit user_posts_path(user)
     expect(page).to have_content('Tom')
     expect(page).to have_css("img[alt='Tom']", count: 1)
@@ -14,19 +16,22 @@ RSpec.feature 'Post Index', type: :feature do
     expect(page).to have_content('Comments: 3')
     expect(page).to have_content('Likes: 1')
   end
-  scenario "see some of the post's title, body and first comments" do
+
+  scenario "see some of the post's title, body, and first comments" do
     visit user_posts_path(user)
     expect(page).to have_content('first text')
     expect(page).to have_content('first comment')
     expect(page).to have_content("first post's title")
   end
+
   scenario 'see a section for pagination if there are more posts than fit on the view' do
     visit user_posts_path(user)
     expect(page).to have_content('Pagination')
   end
-  scenario "clicking on a post, it redirects me to that post's show page" do
+
+  scenario "clicking on a post redirects me to that post's show page" do
     visit user_posts_path(user)
     click_link "first post's title"
-    expect(page).to have_current_path(user_post_path(user, post))
+    expect(page).to have_current_path(post_path(post))
   end
 end
